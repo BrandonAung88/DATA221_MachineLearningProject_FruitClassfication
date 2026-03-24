22749a04123dc86123eee1d90adf32e4c5cb1ee8
chamsA1 <deanchams2005@gmail.com>
2026-03-24 15:39:49 -0600

    Initial data loading for decision tree model

---
+++ Data221FinalProjectDecisionTreeModel.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np
+from PIL import Image
+
+
+def load_images_from_folder(folder):
+    X = []
+    y = []
+
+    for label in os.listdir(folder):
+        class_path = os.path.join(folder, label)
+
+        if not os.path.isdir(class_path):
+            continue
+
+        for img_name in os.listdir(class_path):
+            img_path = os.path.join(class_path, img_name)
+
+            try:
+                img = Image.open(img_path).convert('RGB')
+                img = img.resize((100, 100))
+
+                X.append(np.array(img))
+                y.append(label)
+            except:
+                continue
+
+    return np.array(X), np.array(y)
+
+
+# Load datasets
+X_train, y_train = load_images_from_folder("Project_Train")
+X_val, y_val = load_images_from_folder("Project_Val")
+X_test, y_test = load_images_from_folder("Project_Test")
