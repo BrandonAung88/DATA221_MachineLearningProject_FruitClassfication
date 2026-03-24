18f949bd74c4c1a7e2ae5dec29cea48818a5d0f6
chamsA1 <deanchams2005@gmail.com>
2026-03-24 16:17:53 -0600

    flatten images to make the images readable for a decision tree model

--- Data221FinalProjectDecisionTreeModel.py
+++ Data221FinalProjectDecisionTreeModel.py
@@ -37,3 +37,15 @@ X_test, y_test = load_images_from_folder("Project_Test")
 X_train = X_train / 255.0
 X_val = X_val / 255.0
 X_test = X_test / 255.0
+
+# Flatten images for Decision Tree because decision trees expect a 1d vector as input thus we flatten each image into
+# a 1 dimensional feature vector this converts the 2D image into a format suitable for the model
+# although it removes spatial relationships(how pixels are arranged relative to each other) between pixels which makes it so
+# the program no longer knows which pixels are “next to each other” in the original images. This is a reason why
+# decision trees often perform poorly on raw images compared to CNNs.
+X_train_flat = X_train.reshape(len(X_train), -1)#len(X_train) is the number of images,-1 is a special numpy shortcut to auto find the dimension
+X_val_flat = X_val.reshape(len(X_val), -1)
+X_test_flat = X_test.reshape(len(X_test), -1)
+
+#Since the images are(100, 100, 3) → total pixels = 100 * 100 * 3 = 30,000,
+# So after flattening, each image becomes a 1D vector of length 30,000.
